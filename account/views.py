557ae13a5f7f2372edb5bf0c6f29f2b2d064c1bd
kazem <mohammadkazem2000@gmail.com>
2022-01-02 14:47:49 +0330

    login form and register are created 2

---
+++ account/views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from account.forms import loginform,registerform
+# Create your views here.
+def loginForm(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    form=loginform(request.POST or None)
+    if form.is_valid():
+        username=form.cleaned_data['username']
+        password=form.cleaned_data['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            form.add_error('username','username with this information not found')
+
+    context={
+        "form":form
+    }
+    return render(request,'loginForm.html',context)
+def registerForm(request):
+    form=registerform(request.POST or None)
+    if request.user.is_authenticated:
+        return redirect('/')
+    if form.is_valid():
+        username=form.cleaned_data['username']
+        first_name=form.cleaned_data['first_name']
+        last_name=form.cleaned_data['last_name']
+        email=form.cleaned_data['email']
+        password=form.cleaned_data['password']
+        User.objects.create_user(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
+        user=authenticate(request,username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+        login(request,user)
+        return redirect('/')
+    context={
+        "form":form
+    }
+    return render(request,'registerForm.html',context)
